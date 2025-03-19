5691132206f5cea82d149f414f5f34e4bcfab724
vitorbelarmino <vitor.belarmino@hotmail.com>
2025-03-19 12:42:07 -0300

    feat: add validation token to acreate task

--- src/Utils/Token.ts
+++ src/Utils/Token.ts
@@ -1,4 +1,6 @@
-import jwt from "jsonwebtoken";
+import jwt, { TokenExpiredError } from "jsonwebtoken";
+import { CustomError } from "./CustomError";
+import { Request } from "express";
 
 interface IUser {
   email: string;
@@ -14,5 +16,35 @@ export class Token {
       algorithm: "HS256",
     });
     return token;
+  }
+  static async verifyToken(token: string) {
+    try {
+      const user = jwt.verify(token, process.env.JWT_SECRET || "senha");
+      return user;
+    } catch (error) {
+      if (error instanceof TokenExpiredError) {
+        throw new CustomError(401, error.message);
+      }
+
+      if (error instanceof jwt.JsonWebTokenError) {
+        throw new CustomError(401, error.message);
+      }
+
+      throw new CustomError(500, "Internal Server Error");
+    }
+  }
+
+  static async validateToken(req: Request) {
+    const { authorization } = req.headers;
+    if (!authorization) {
+      throw new CustomError(401, "Token not found");
+    }
+
+    const [, token] = authorization.split(" ");
+    if (!token) {
+      throw new CustomError(401, "Token not found");
+    }
+    const user = await Token.verifyToken(token);
+    return user;
   }
 }
--- src/middlewares/TaskMiddleware.ts
+++ src/middlewares/TaskMiddleware.ts
@@ -1,10 +1,12 @@
 import { Request, Response, NextFunction } from "express";
 import { createTaskSchema } from "../schemas/TaskSchemas";
+import { Token } from "../Utils/Token";
 
 export class TaskMiddleware {
   static async create(req: Request, res: Response, next: NextFunction) {
     const { body } = req;
 
+    await Token.validateToken(req);
     const { error } = createTaskSchema.validate(body);
 
     if (error) {
